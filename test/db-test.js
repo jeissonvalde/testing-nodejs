56419c428679d28c82a039c522e9b458c924b926
Jeisson Andrés <jeissonvalderramam@gmail.com>
2016-12-25 19:16:53 -0500

    Optimizando la rapidez y organización del setup para producción. Busqueda de imagenes por usuario y tags

--- test/db-test.js
+++ test/db-test.js
@@ -9,7 +9,7 @@ const utils = require('../lib/utils')
 
 test.beforeEach('setup database', async t => {
   const dbName = `platzigram_${uuid.v4()}`
-  const db = new Db({ db: dbName })
+  const db = new Db({ db: dbName, setup: true })
   await db.connect()
   t.context.db = db
   t.context.dbName = dbName
@@ -129,7 +129,7 @@ test('authenticate user', async t => {
   t.false(failure)
 })
 
-test('list images by  user', async t => {
+test('list images by user', async t => {
   let db = t.context.db
 
   t.is(typeof db.getImagesByUser, 'function', 'getImagesByUser is a function')
@@ -150,5 +150,29 @@ test('list images by  user', async t => {
   await Promise.all(saveImages)
 
   let result = await db.getImagesByUser(userId)
+  t.is(result.length, random)
+})
+
+test('list images by tag', async t => {
+  let db = t.context.db
+
+  t.is(typeof db.getImagesByTag, 'function', 'getImagesByTag is a function')
+
+  let images = fixtures.getImages(10)
+  let tag = '#filterit'
+  let random = Math.round(Math.random() * images.length)
+
+  let saveImages = []
+  for (let i = 0; i < images.length; i++) {
+    if (i < random) {
+      images[i].description = tag
+    }
+
+    saveImages.push(db.saveImage(images[i]))
+  }
+
+  await Promise.all(saveImages)
+
+  let result = await db.getImagesByTag(tag)
   t.is(result.length, random)
 })
